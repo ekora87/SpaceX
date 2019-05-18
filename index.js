b63efd263495907bef25938d32b446a8014c0539
ekora87 <ekora87@gmail.com>
2019-05-18 11:46:06 -0500

    add mission.html

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//let state = getMissionPatch();
+let local = localStorage.getItem('index');
 function getMissionPatch() {
     fetch('https://api.spacexdata.com/v3/launches')
     .then(response => response.json())
@@ -7,29 +9,33 @@ function getMissionPatch() {
   function displayMissionPatch(responseJson) {
     for (let i=0; i<responseJson.length; i++ ){
       if (responseJson[i].links.mission_patch !== null) {
-       $('.mission').append(`<div class="mission-container" id="${responseJson[i].flight_number - 1}"><h3 class="flight-number">Flight Number ${responseJson[i].flight_number}</h3><h3 class="mission-name">${responseJson[i].mission_name}</h3><img src="${responseJson[i].links.mission_patch}" class="mission-patch"></div>`);
+       $('.mission').append(`<a href="mission.html"><div class="mission-container" id="${responseJson[i].flight_number - 1}"><h3 class="flight-number">Flight Number ${responseJson[i].flight_number}</h3><h3 class="mission-name">${responseJson[i].mission_name}</h3><img src="${responseJson[i].links.mission_patch}" class="mission-patch"></div></a>`);
     }
     }
   }
-  
+
+
+// function initialize() {
+//   window.history.replaceState(state, null, "");
+//   render(state);
+// };
+ 
+
   function watchMissionPatch() {
     
-    $('.mission').on('click', '.mission-container', function(event) {
+    $('.mission').on('click', '.mission-container', function() {
        let num = $(this).attr('id');
+       
       $('.mission').hide();
-      //$('.individual-mission').show();
-      getIndividualMission(num);
-      //alert(num);
+      local = localStorage.setItem('index', num);
     });  
   }
-  
-  
-  function getIndividualMission(index) {
+ 
+  function getIndividualMission(local) {
     fetch('https://api.spacexdata.com/v3/launches')
     .then(response => response.json())
-    .then(responseJson => displayMissionDetail(responseJson[index]));
+    .then(responseJson => displayMissionDetail(responseJson[parseInt(local)]));
   }
-  
   
   function displayMissionDetail(responseJson) {
     if (responseJson.links.flickr_images.length > 0) {
@@ -37,23 +43,47 @@ function getMissionPatch() {
         $('.mission-images').append(`<img src="${responseJson.links.flickr_images[i]}">`);
       }
     }
-    $('.mission-details').append(`<h3 class="flight-number">${responseJson.mission_name}</h3><ul><li><span class="bold-text">Launch Year:</span> ${responseJson.launch_year}</li><li><span class="bold-text">Launch Date:</span> ${responseJson.launch_date_local}</li><li><span class="bold-text">Rocket Name:</span> ${responseJson.rocket.rocket_name}</li><li><span class="bold-text">Launch Site:</span> ${responseJson.launch_site.site_name_long}</li>
-    <li><span class="bold-text">Launch Sucess:</span> ${responseJson.launch_success}</li><li><span class="bold-text">Details:</span> ${responseJson.details}</li></ul>`);
+    $('.mission-details').append(`
+    <h3 class="flight-number">${responseJson.mission_name}</h3>
+    <ul><li><span class="bold-text">Flight Numer:</span> ${responseJson.flight_number}</li>
+    <li><span class="bold-text">Launch Year:</span> ${responseJson.launch_year}</li>
+    <li><span class="bold-text">Launch Date:</span> ${responseJson.launch_date_local}</li>
+    <li><span class="bold-text">Rocket Name:</span> ${responseJson.rocket.rocket_name}</li>
+    <li><span class="bold-text">Launch Site:</span> ${responseJson.launch_site.site_name_long}</li>
+    <li><span class="bold-text">Launch Sucess:</span> ${responseJson.launch_success}</li>
+    <li><span class="bold-text">Video:</span> <a href="${responseJson.links.video_link}" target="_blank">YouTube Video</a></li>
+    <li><span class="bold-text">Details:</span> ${responseJson.details}</li>
+    </ul>`);
   }
   
-  function back() {
-    $('.aTag').click(function() {
-      event.preventDefault();
-      $('.mission').show();
-      $('.mission-images').empty();
-      $('.mission-details').empty();
+  // function back() {
+  //   $('.aTag').click(function() {
+  //     event.preventDefault();
+  //     $('.mission').show();
+  //     $('.mission-images').empty();
+  //     $('.mission-details').empty();
       
-    });
-  }
+  //   });
+  // }
+
+  // function handleButtonClick() {
+  //   state = watchMissionPatch();
+  //   window.history.pushState(state, null, "");
+  //   render(state);
+  // }
+
+  
+window.onpopstate = function (event) {
+  if (event.state) { state = event.state; }
+  render(state);
+};
   
   $(function() {
+    //initialize();
+    
     getMissionPatch();
+    
     watchMissionPatch();
-    back();
-    //displayMissionDetail();
+    getIndividualMission(local);
+    
   })
