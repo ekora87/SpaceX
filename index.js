a6a9dd56222cad0bb4a189e1033c3e456bac15dd
Beyonce Knowles <beyonce@thinkful.com>
2019-05-21 06:41:28 -0500

    slideshow added

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
 //let state = getMissionPatch();
 let local = localStorage.getItem('index');
+let slideIndex = 1;
 function getMissionPatch() {
     fetch('https://api.spacexdata.com/v3/launches')
     .then(response => response.json())
     .then(responseJson => displayMissionPatch(responseJson));
   }
   
-  function displayMissionPatch(responseJson) {
-    
+function displayMissionPatch(responseJson) {
     for (let i=0; i < responseJson.length; i++ ){
       if (responseJson[i].links.mission_patch !== null) {
-       $('.mission').append(`<a href="mission.html"><div class="mission-container" id="${responseJson[i].flight_number - 1}"><h3 class="flight-number">Flight Number ${responseJson[i].flight_number}</h3><h4 class="mission-name">${responseJson[i].mission_name}</h4><img src="${responseJson[i].links.mission_patch}" class="mission-patch"></div></a>`);
+        $('.mission').append(`<a href="mission.html"><div class="mission-container" id="${responseJson[i].flight_number - 1}"><h3 class="flight-number">Flight Number ${responseJson[i].flight_number}</h3><h4 class="mission-name">${responseJson[i].mission_name}</h4><img src="${responseJson[i].links.mission_patch}" class="mission-patch"></div></a>`);
+        }
     }
-    }
-    
-  }
-
-
-// function initialize() {
-//   window.history.replaceState(state, null, "");
-//   render(state);
-// };
+}
  
 
   function watchMissionPatch() {
-    
     $('.mission').on('click', '.mission-container', function() {
-       let num = $(this).attr('id');
-       
-      $('.mission').hide();
-      local = localStorage.setItem('index', num);
+        let num = $(this).attr('id');
+        $('.mission').hide();
+        local = localStorage.setItem('index', num);
     });  
   }
  
@@ -40,13 +31,23 @@ function getMissionPatch() {
   }
   
   function displayMissionDetail(responseJson) {
-    if (responseJson.links.flickr_images.length > 0) {
-      for (let i=0; i < responseJson.links.flickr_images.length; i++) {
-        $('.mission-images').append(`<img src="${responseJson.links.flickr_images[i]}">`);
+    $('.mission-images').append(`<div class="slide-control"><a class="prev" onclick="plusSlides(-1)">PREV</a>
+    <span class="numbertext"></span>
+    <a class="next" onclick="plusSlides(1)">NEXT</a></div>`);
+    let imageCount = responseJson.links.flickr_images.length;
+    if (imageCount > 0) {
+      for (let i=0; i < imageCount; i++) {
+        
+        $('.mission-images').append(`<div class="mySlides fade">
+        <img class="images" src="${responseJson.links.flickr_images[i]}">
+        </div>
+
+        `);
       }
     }
-    $('.mission-details').append(`
-    <h3 class="flight-number">${responseJson.mission_name}</h3>
+    showSlides(slideIndex);
+    $('.individual-mission').prepend(`<div class="mission-details">
+    <h3 class="name">${responseJson.mission_name}</h3>
     <ul><li><span class="bold-text">Flight Numer:</span> ${responseJson.flight_number}</li>
     <li><span class="bold-text">Launch Year:</span> ${responseJson.launch_year}</li>
     <li><span class="bold-text">Launch Date:</span> ${responseJson.launch_date_local}</li>
@@ -55,37 +56,47 @@ function getMissionPatch() {
     <li><span class="bold-text">Launch Sucess:</span> ${responseJson.launch_success}</li>
     <li><span class="bold-text">Video:</span> <a href="${responseJson.links.video_link}" target="_blank">YouTube Video</a></li>
     <li><span class="bold-text">Details:</span> ${responseJson.details}</li>
-    </ul>`);
+    </ul></div>`);
   }
   
-  // function back() {
-  //   $('.aTag').click(function() {
-  //     event.preventDefault();
-  //     $('.mission').show();
-  //     $('.mission-images').empty();
-  //     $('.mission-details').empty();
-      
-  //   });
-  // }
+// window.onpopstate = function (event) {
+//   if (event.state) { state = event.state; }
+//   render(state);
+// };
 
-  // function handleButtonClick() {
-  //   state = watchMissionPatch();
-  //   window.history.pushState(state, null, "");
-  //   render(state);
-  // }
 
-  
-window.onpopstate = function (event) {
-  if (event.state) { state = event.state; }
-  render(state);
-};
+
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+      slideIndex = 1;
+      n = 1;
+    }    
+  if (n < 1) {
+      slideIndex = slides.length;
+      n = slides.length;
+    }
+  for (let i =0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//   }
+slides[slideIndex - 1].style.display = "block";  
+$('.numbertext').text(`${n}/${slides.length}`);
+//   dots[slideIndex-1].className += " active";
+}
   
   $(function() {
-    //initialize();
-    
     getMissionPatch();
-    
     watchMissionPatch();
     getIndividualMission(local);
-    
+    showSlides(slideIndex);
   })
